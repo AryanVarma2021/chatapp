f61e48065c7c8b4270fa4625f71c6f7f7a969912
AryanVarma2021 <varmaaryan11@gmail.com>
2024-02-02 22:13:27 +0530

    some changes in myStyle.css file

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
 
-const ChatArea = () => {
+const ChatArea = ({props}) => {
   return (
     <div className='chatArea-container'>
-        <div className='chatArea-header'>ChatArea-Header</div>
-        <div className='messages-conatiner'>Messages Conatiner</div>
-        <div className='text-input-area'>Input-text-area</div>
+        <div className='chatArea-header'>
+          <p className='con-icon'>{props.name[0]}</p>
+          <div className='header-text'>
+            <p className='con-title'>{props.name}</p>
+            <p className='con-timeStamp'>{props.timeStamp}</p>
+
+          </div>
+          <IconButton>
+            <DeleteIcon/>
+          </IconButton>
+        </div>
+        <div className='messages-container'>Messages Conatiner</div>
+        <div className='text-input-area'>
+          <input placeholder='Type a message' className='search-box' type='text' />
+          <IconButton>
+            <SendIcon/>
+          </IconButton>
+        </div>
     </div>
   )
 }
